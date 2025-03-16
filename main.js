5a61c1cf1c834b4d1228ba71dc6fd282b1dd3f8e
parsirani <kamranatabaki28@gmail.com>
2025-03-16 23:58:50 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,208 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let score = loadScore();
+    let energy = loadEnergy();
+    let maxEnergy = loadMaxEnergy();
+    let refillSpeedLevel = loadRefillSpeedLevel();
+    const scoreElement = document.getElementById('score');
+    const goldenImage = document.getElementById('golden-image');
+    const rankText = document.getElementById('rank-text');
+    const energyElement = document.getElementById('energy');
+    const maxEnergyElement = document.getElementById('max-energy');
+    const energyBar = document.getElementById('energy-bar');
+    const bodyElement = document.body;
+
+    scoreElement.textContent = score;
+    energyElement.textContent = energy;
+    maxEnergyElement.textContent = `/${maxEnergy}`;
+    updateEnergyBar();
+
+    goldenImage.addEventListener('click', handleClick);
+
+    function handleClick() {
+        let multiHitLevel = parseInt(localStorage.getItem('multiHitLevel')) || 1;
+        if (energy >= multiHitLevel) {
+            let increment = 1;
+            if (localStorage.getItem('boost') === 'turbo-active') {
+                increment = 5;
+            }
+            increment *= multiHitLevel;
+
+            score += increment;
+            if (localStorage.getItem('boost') !== 'turbo-active') {
+                energy -= multiHitLevel;
+            }
+            scoreElement.textContent = score;
+            energyElement.textContent = energy;
+            updateEnergyBar();
+            updateRankText(score);
+            saveScore(score);
+            saveEnergy(energy);
+            goldenImage.style.transform = 'scale(0.9)';
+            setTimeout(() => {
+                goldenImage.style.transform = 'scale(1)';
+            }, 100);
+        }
+    }
+
+    function updateRankText(score) {
+        if (score < 1000) {
+            rankText.innerHTML = 'لیگ نوح (ع) 🥉'; // اضافه کردن ایموجی برنز
+            bodyElement.style.background = 'linear-gradient(180deg,rgb(224, 223, 124),rgb(35, 161, 63))';
+            energyBar.style.backgroundColor = '#cd7f32'; /* رنگ نوار شارژ برای لیگ برنزی */
+        } else if (score < 5000) {
+            rankText.innerHTML = 'لیگ ادریس (ع) 🥈'; // اضافه کردن ایموجی نقره
+            bodyElement.style.background = 'linear-gradient(180deg,rgba(170, 233, 238, 0.83),rgb(35, 161, 63))';
+            energyBar.style.backgroundColor = '#c0c0c0'; /* رنگ نوار شارژ برای لیگ نقره‌ای */
+        } else if (score < 20000) {
+            rankText.innerHTML = 'لیگ موسی (ع) 🥇'; // اضافه کردن ایموجی طلا
+            bodyElement.style.background = 'linear-gradient(180deg,rgba(236, 214, 14, 0.8),rgb(35, 161, 63))';
+            energyBar.style.backgroundColor = '#f9a602'; /* رنگ نوار شارژ برای لیگ طلایی */
+        } else {
+            rankText.innerHTML = 'لیگ یوسف عزیز مصر (ع) 💎'; // اضافه کردن ایموجی الماس
+            bodyElement.style.background = 'linear-gradient(180deg,rgb(71, 103, 172),rgb(35, 161, 63))';
+            energyBar.style.backgroundColor = '#b9f2ff'; /* رنگ نوار شارژ برای لیگ الماس */
+        }
+    }
+
+    function updateEnergyBar() {
+        const energyPercentage = (energy / maxEnergy) * 100;
+        energyBar.style.width = energyPercentage + '%';
+    }
+
+    // شارژ انرژی بر اساس سرعت پرشدن تسبیح
+    setInterval(() => {
+        if (energy < maxEnergy) {
+            energy += refillSpeedLevel;
+            if (energy > maxEnergy) {
+                energy = maxEnergy;
+            }
+            energyElement.textContent = energy;
+            updateEnergyBar();
+            saveEnergy(energy);
+        }
+    }, 1000);
+
+    // نمایش پیام خاص وقتی انرژی به 0 رسید
+    function showSpecialMessage() {
+        goldenImage.textContent = "صلوات شما به حد نصاب رسید";
+        goldenImage.classList.add('special-message');
+        setTimeout(() => {
+            goldenImage.textContent = "امام";
+            goldenImage.classList.remove('special-message');
+        }, 3000);
+    }
+
+    // نظارت بر انرژی و نمایش پیام خاص
+    setInterval(() => {
+        if (energy === 0) {
+            showSpecialMessage();
+        }
+    }, 1000);
+
+    // ذخیره‌سازی امتیاز
+    function saveScore(score) {
+        localStorage.setItem('userScore', score);
+    }
+
+    // بازیابی امتیاز
+    function loadScore() {
+        return parseInt(localStorage.getItem('userScore')) || 0;
+    }
+
+    // ذخیره‌سازی انرژی
+    function saveEnergy(energy) {
+        localStorage.setItem('userEnergy', energy);
+    }
+
+    // بازیابی انرژی
+    function loadEnergy() {
+        return parseInt(localStorage.getItem('userEnergy')) || 150;
+    }
+
+    // ذخیره‌سازی حداکثر انرژی
+    function saveMaxEnergy(maxEnergy) {
+        localStorage.setItem('maxEnergy', maxEnergy);
+    }
+
+    // بازیابی حداکثر انرژی
+    function loadMaxEnergy() {
+        return parseInt(localStorage.getItem('maxEnergy')) || 150;
+    }
+
+    // ذخیره‌سازی لول سرعت پرشدن تسبیح
+    function saveRefillSpeedLevel(level) {
+        localStorage.setItem('increaseRefillSpeedLevel', level);
+    }
+
+    // بازیابی لول سرعت پرشدن تسبیح
+    function loadRefillSpeedLevel() {
+        return parseInt(localStorage.getItem('increaseRefillSpeedLevel')) || 1;
+    }
+
+    // به‌روزرسانی رنگ پس‌زمینه و متن لیگ هنگام بارگذاری صفحه
+    updateRankText(score);
+    updateEnergyBar();
+
+    // هدایت به صفحه mos.html هنگام کلیک بر روی مستحبات
+    document.getElementById('mostahabat-link').addEventListener('click', () => {
+        window.location.href = 'mos.html';
+    });
+
+    document.getElementById('task-link').addEventListener('click', () => {
+        window.location.href = 'task.html';
+    });
+
+    // بررسی بوسترها
+    function checkBoosters() {
+        const boost = localStorage.getItem('boost');
+        if (boost === 'full-energy') {
+            energy = maxEnergy;
+            localStorage.removeItem('boost');
+            updateEnergyBar();
+            energyElement.textContent = energy;
+            saveEnergy(energy);
+        } else if (boost === 'turbo') {
+            // افزایش ماین کردن به 5 برای 10 ثانیه
+            let multiHitLevel = parseInt(localStorage.getItem('multiHitLevel')) || 1;
+            localStorage.setItem('boost', 'turbo-active');
+            goldenImage.classList.add('turbo-boost');
+            setTimeout(() => {
+                localStorage.removeItem('boost');
+                goldenImage.classList.remove('turbo-boost');
+            }, 10000);
+        }
+    }
+
+    // بررسی بوسترها در هر بار بارگذاری صفحه
+    checkBoosters();
+
+    // محاسبه سکه‌های اضافی بر اساس زمان گذشته
+    function calculateAdditionalCoins() {
+        const lastVisit = parseInt(localStorage.getItem('lastVisit')) || new Date().getTime();
+        const now = new Date().getTime();
+        const minutesPassed = Math.floor((now - lastVisit) / (1000 * 60));
+        const secondsPassed = Math.floor((now - lastVisit) / 1000);
+        const additionalCoins = minutesPassed * 10;
+        const additionalEnergy = Math.min(secondsPassed, maxEnergy - energy);
+        const balance = parseInt(localStorage.getItem('userScore')) || 0;
+
+        if (localStorage.getItem('divineQueenActive') === 'true') {
+            localStorage.setItem('userScore', balance + additionalCoins);
+        }
+
+        energy += additionalEnergy;
+        energyElement.textContent = energy;
+        updateEnergyBar();
+        saveEnergy(energy);
+
+        localStorage.setItem('lastVisit', now);
+    }
+
+    // ذخیره‌سازی انرژی هنگام بستن صفحه
+    window.addEventListener('beforeunload', () => {
+        saveEnergy(energy);
+    });
+
+    // فراخوانی تابع محاسبه سکه‌های اضافی هنگام بارگذاری صفحه
+    calculateAdditionalCoins();
+});
